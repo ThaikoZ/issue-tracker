7c6aa9d49505ee100ec7f48f11a09102e62d0795
Adrian Sudak <adriansudak@outlook.com>
2023-11-23 12:23:07 +0100

    Add spinner

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "@/prisma/client";
+import { createIssueSchema } from "../../validationSchemas";
 
-const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is requierd").max(255),
-  description: z.string().min(1, "Description is requierd"),
-});
-
-export async function GET(request: NextRequest) {
-  const issues = await prisma.issue.findMany();
-  return NextResponse.json(issues, { status: 200 });
-}
+// export async function GET(request: NextRequest) {
+//   const issues = await prisma.issue.findMany();
+//   return NextResponse.json(issues, { status: 200 });
+// }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,32 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { SubmitHandler, Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { CiWarning } from "react-icons/ci";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const router = useRouter();
   const [error, setError] = useState<string>();
+  const [isSubmitting, setSubmitting] = useState<boolean>(false);
 
   return (
     <div className="max-w-xl ">
@@ -32,9 +42,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmitting(false);
             setError("An unexpeced error occured.");
           }
         })}
@@ -42,6 +54,7 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
         <Controller
           name="description"
           control={control}
@@ -49,8 +62,10 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
-        <Button>Submit new issue</Button>
+        {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
+        <Button disabled={isSubmitting}>
+          Submit new issue{isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
