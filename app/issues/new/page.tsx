e070f9343a82a47876631ce6b3398c02e73dbde1
Adrian Sudak <adriansudak@outlook.com>
2023-11-23 12:27:35 +0100

    Refactor submitting and fix alert

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -28,6 +28,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState<string>();
   const [isSubmitting, setSubmitting] = useState<boolean>(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpeced error occured.");
+    }
+  });
+
   return (
     <div className="max-w-xl ">
       {error && (
@@ -38,19 +49,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpeced error occured.");
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" radius="large">
+        <Theme radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
           {/* <ThemePanel /> */}
