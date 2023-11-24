1e18507b7d7cd89e16164932bc397185b1c536e8
Adrian Sudak <adriansudak@outlook.com>
2023-11-24 19:58:52 +0100

    Show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,27 +1,31 @@
-import { Button, Dialog } from "@radix-ui/themes";
+"use client";
+import { Button, AlertDialog, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    // <Dialog.Root>
-    //     <Dialog.Trigger>
-    <Button color="red">Delete Issue</Button>
-    //    </Dialog.Trigger>
-    //   <Dialog.Content style={{ maxWidth: 450 }}>
-    //     <Dialog.Title>Delete Issue</Dialog.Title>
-    //     <Dialog.Description size="2" mb="4">
-    //       Are you sure you want to delete this issue?
-    //     </Dialog.Description>
-    //     <Dialog.Close>
-    //       <Dialog.Close>
-    //         <Button variant="soft" color="gray">
-    //           Cancel
-    //         </Button>
-    //       </Dialog.Close>
-    //       <Button color="red">Delete</Button>
-    //     </Dialog.Close>
-    //   </Dialog.Content>
-    // </Dialog.Root>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description size="2" mb="4">
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="2" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Cancel>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Cancel>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
