30d75f4504807c9c2be7dd8e85b7aca69f15e97d
Adrian Sudak <adriansudak@outlook.com>
2023-11-24 19:45:57 +0100

    Repair sizing

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,27 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Dialog } from "@radix-ui/themes";
 import Link from "next/link";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
-    </Button>
+    // <Dialog.Root>
+    //     <Dialog.Trigger>
+    <Button color="red">Delete Issue</Button>
+    //    </Dialog.Trigger>
+    //   <Dialog.Content style={{ maxWidth: 450 }}>
+    //     <Dialog.Title>Delete Issue</Dialog.Title>
+    //     <Dialog.Description size="2" mb="4">
+    //       Are you sure you want to delete this issue?
+    //     </Dialog.Description>
+    //     <Dialog.Close>
+    //       <Dialog.Close>
+    //         <Button variant="soft" color="gray">
+    //           Cancel
+    //         </Button>
+    //       </Dialog.Close>
+    //       <Button color="red">Delete</Button>
+    //     </Dialog.Close>
+    //   </Dialog.Content>
+    // </Dialog.Root>
   );
 };
 
