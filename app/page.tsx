682570e6504f58b9ca2b6782ee807b925c038d1a
Adrian Sudak <adriansudak@outlook.com>
2023-11-26 22:56:47 +0100

    Build the latest issue tracker

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
 import { Button } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
+import LastestIssues from "./LastestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+export default function Home() {
+  return <LastestIssues />;
 }
